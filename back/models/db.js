53b5c25da969480121e87050711beb6434cc005d
Pollez Laurent <laurent.pollez@gmail.com>
2020-04-28 18:29:06 +0200

    add trailer movie, env var

--- back/models/db.js
+++ back/models/db.js
@@ -1,13 +1,12 @@
+require('dotenv').config();
 var mongoose = require('mongoose');
 
 var options = {
   connectTimeoutMS: 5000,
-  useNewUrlParser: true // deprecated à terme
-}
+  useNewUrlParser: true, // deprecated à terme
+};
 
-mongoose.connect('mongodb://test:test35@ds263109.mlab.com:63109/mymovizapp',
-    options,
-    function(err) { // cette fonction s'execute à la connexion (erreur ou pas)
-      console.log(err);
-    }
-);
+mongoose.connect(process.env.DB_NAME, options, function (err) {
+  // cette fonction s'execute à la connexion (erreur ou pas)
+  console.log(err);
+});
